879a08bf370a588d80a2608edef2aedcb16ec197
iffanahmad98 <iffanahmad98@gmail.com>
2020-12-20 21:53:35 +0000

    Initial praktikum 9

---
+++ praktikumPpb/praktikum9/src/main/java/com/iffan_18102125/praktikum9/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.iffan_18102125.praktikum9
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.iffan_18102125.praktikum9.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.title = getString(R.string.main_title)
+
+    }
+}
