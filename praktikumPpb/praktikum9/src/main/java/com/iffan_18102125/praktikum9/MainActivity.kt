c9dab24eab901ce6454e8896f01f73048f34fe0c
iffanahmad98 <iffanahmad98@gmail.com>
2020-12-24 08:59:15 +0000

    move setting button

--- praktikumPpb/praktikum9/src/main/java/com/iffan_18102125/praktikum9/MainActivity.kt
+++ praktikumPpb/praktikum9/src/main/java/com/iffan_18102125/praktikum9/MainActivity.kt
@@ -2,6 +2,8 @@ package com.iffan_18102125.praktikum9
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import com.iffan_18102125.praktikum9.data.SettingModel
@@ -52,6 +54,22 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == REQUEST_CODE) {
             if (resultCode == SettingPreferenceActivity.RESULT_CODE) {
                 showExistingPreference()
+            }
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.action_settings -> {
+                val intent = Intent(this@MainActivity, SettingPreferenceActivity::class.java)
+                startActivityForResult(intent, REQUEST_CODE)
             }
         }
     }
