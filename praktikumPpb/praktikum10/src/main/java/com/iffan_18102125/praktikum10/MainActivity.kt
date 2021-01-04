7740a7b989ce2365c303834f558bb3e4c7473e85
iffanahmad98 <iffanahmad98@gmail.com>
2021-01-04 22:13:01 +0000

    add code in item_quote.xml, MainActivity.kt, & QuoteAdapter.kt

--- praktikumPpb/praktikum10/src/main/java/com/iffan_18102125/praktikum10/MainActivity.kt
+++ praktikumPpb/praktikum10/src/main/java/com/iffan_18102125/praktikum10/MainActivity.kt
@@ -1,10 +1,19 @@
 package com.iffan_18102125.praktikum10
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.iffan_18102125.praktikum10.adapter.QuoteAdapter
+import com.iffan_18102125.praktikum10.data.Quote
 import com.iffan_18102125.praktikum10.databinding.ActivityMainBinding
 import com.iffan_18102125.praktikum10.db.QuoteHelper
+import com.iffan_18102125.praktikum10.helper.mapCursorToArrayList
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var quoteHelper: QuoteHelper
@@ -14,6 +23,49 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "Quotes"
+        binding.rvQuotes.layoutManager = LinearLayoutManager(this)
+        binding.rvQuotes.setHasFixedSize(true)
+        adapter = QuoteAdapter(this)
+        binding.rvQuotes.adapter = adapter
+
+        quoteHelper = QuoteHelper.getInstance(applicationContext)
+        quoteHelper.open()
+        if (savedInstanceState == null) {
+            loadQuotes()
+        } else {
+            val list = savedInstanceState.getParcelableArrayList<Quote>(EXTRA_STATE)
+            if (list != null) {
+                adapter.listQuotes = list
+            }
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelableArrayList(EXTRA_STATE, adapter.listQuotes)
+    }
+
+    private fun loadQuotes() {
+        GlobalScope.launch(Dispatchers.Main) {
+            progressbar.visibility = View.VISIBLE
+            val cursor = quoteHelper.queryAll()
+            var quotes = mapCursorToArrayList(cursor)
+            progressbar.visibility = View.INVISIBLE
+            if (quotes.size > 0) {
+                adapter.listQuotes = quotes
+            } else {
+                adapter.listQuotes = ArrayList()
+                showSnackbarMessage("Tidak ada data saat ini")
+            }
+        }
+    }
+
+    private fun showSnackbarMessage(message: String) {
+        Snackbar.make(binding.rvQuotes, message, Snackbar.LENGTH_SHORT).show()
+    }
+
 }
