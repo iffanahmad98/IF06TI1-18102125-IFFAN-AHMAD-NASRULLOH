16928d3d412499aee8fb283405d585b511d877c8
iffanahmad98 <iffanahmad98@gmail.com>
2021-01-04 22:25:18 +0000

    Create QuoteAddUpdateActivity.kt

---
+++ praktikumPpb/praktikum10/src/main/java/com/iffan_18102125/praktikum10/QuoteAddUpdateActivity.kt
@@ -0,0 +1,11 @@
+package com.iffan_18102125.praktikum10
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class QuoteAddUpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quote_add_update)
+    }
+}
