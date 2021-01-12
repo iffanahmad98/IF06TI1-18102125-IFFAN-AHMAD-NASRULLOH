82c21c7a5fb417b1c84aecc69c5350c534f83978
iffanahmad98 <iffanahmad98@gmail.com>
2021-01-12 13:10:02 +0000

    inisialisasi GoogleSignInOptions in SignInActivity.kt

--- praktikumPpb/praktikum11/src/main/java/com/iffan_18102125/praktikum11/SignInActivity.kt
+++ praktikumPpb/praktikum11/src/main/java/com/iffan_18102125/praktikum11/SignInActivity.kt
@@ -6,7 +6,9 @@ import android.text.TextUtils
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -27,6 +29,11 @@ class SignInActivity : AppCompatActivity(), View.OnClickListener {
         binding.btnPhone.setOnClickListener(this)
         auth = Firebase.auth
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 
     override fun onClick(v: View) {
