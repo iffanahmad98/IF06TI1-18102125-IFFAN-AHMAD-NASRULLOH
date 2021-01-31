effdc50b7570f484236e00c9d861ab16394d7f8f
iffanahmad98 <iffanahmad98@gmail.com>
2021-01-31 13:43:44 +0000

    Initial praktikum14

---
+++ praktikumPpb/praktikum14/src/main/java/com/iffan_18102125/praktikum14/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.iffan_18102125.praktikum14.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ praktikumPpb/praktikum14/src/main/java/com/iffan_18102125/praktikum14/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.iffan_18102125.praktikum14.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
