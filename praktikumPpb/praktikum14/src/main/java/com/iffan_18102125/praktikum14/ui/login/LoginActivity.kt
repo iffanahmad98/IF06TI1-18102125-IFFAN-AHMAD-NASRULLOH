9cad2c548ef4c9a1cfd352ebe1f5d7765d9064d6
iffanahmad98 <iffanahmad98@gmail.com>
2021-01-31 21:36:40 +0000

    create login activity

---
+++ praktikumPpb/praktikum14/src/main/java/com/iffan_18102125/praktikum14/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.iffan_18102125.praktikum14.ui.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.iffan_18102125.praktikum14.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
