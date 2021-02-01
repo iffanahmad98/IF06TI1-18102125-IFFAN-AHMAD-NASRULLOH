8422953d8050bcedda539f10386335b801b18cbe
iffanahmad98 <iffanahmad98@gmail.com>
2021-02-01 20:28:48 +0000

    create MainView.kt & CoroutineContextProvider.kt

---
+++ praktikumPpb/praktikum14/src/main/java/com/iffan_18102125/praktikum14/CoroutineContextProvider.kt
@@ -0,0 +1,8 @@
+package com.iffan_18102125.praktikum14
+
+import kotlinx.coroutines.Dispatchers
+import kotlin.coroutines.CoroutineContext
+
+open class CoroutineContextProvider {
+    open val main: CoroutineContext by lazy { Dispatchers.Main }
+}
