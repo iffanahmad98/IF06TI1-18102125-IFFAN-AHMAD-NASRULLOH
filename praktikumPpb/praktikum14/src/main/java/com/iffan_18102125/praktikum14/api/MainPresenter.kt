eab67dec70065122274ca5940cc322e715be8e36
iffanahmad98 <iffanahmad98@gmail.com>
2021-02-01 21:19:35 +0000

    create MainPresenter.kt

---
+++ praktikumPpb/praktikum14/src/main/java/com/iffan_18102125/praktikum14/api/MainPresenter.kt
@@ -0,0 +1,8 @@
+package com.iffan_18102125.praktikum14.api
+
+import com.iffan_18102125.praktikum14.CoroutineContextProvider
+import com.iffan_18102125.praktikum14.`interface`.MainView
+
+class MainPresenter(private val view: MainView, private val context:
+CoroutineContextProvider = CoroutineContextProvider()) {
+}
