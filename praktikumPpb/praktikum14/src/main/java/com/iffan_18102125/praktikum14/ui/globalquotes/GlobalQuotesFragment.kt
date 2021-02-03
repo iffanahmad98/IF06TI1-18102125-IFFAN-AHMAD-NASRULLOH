af5d3f6735e73e66bc6b00882cb0ef2ea6a1610f
iffanahmad98 <iffanahmad98@gmail.com>
2021-02-03 08:39:10 +0000

    Add code to read data

--- praktikumPpb/praktikum14/src/main/java/com/iffan_18102125/praktikum14/ui/globalquotes/GlobalQuotesFragment.kt
+++ praktikumPpb/praktikum14/src/main/java/com/iffan_18102125/praktikum14/ui/globalquotes/GlobalQuotesFragment.kt
@@ -4,28 +4,68 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.iffan_18102125.praktikum14.CoroutineContextProvider
+import com.iffan_18102125.praktikum14.QuoteAdapter
 import com.iffan_18102125.praktikum14.R
+import com.iffan_18102125.praktikum14.TokenPref
+import com.iffan_18102125.praktikum14.`interface`.MainView
+import com.iffan_18102125.praktikum14.api.MainPresenter
+import com.iffan_18102125.praktikum14.databinding.FragmentGlobalQuotesBinding
+import com.iffan_18102125.praktikum14.model.Login
+import com.iffan_18102125.praktikum14.model.Quote
+import com.iffan_18102125.praktikum14.model.Token
+import kotlinx.android.synthetic.main.fragment_my_quotes.*
+import org.jetbrains.anko.support.v4.onRefresh
 
-class GlobalQuotesFragment : Fragment() {
+ class GlobalQuotesFragment : Fragment(), MainView {
+        private lateinit var presenter: MainPresenter
+        private var quotes: MutableList<Quote> = mutableListOf()
+        private lateinit var adapter: QuoteAdapter
+        private lateinit var tokenPref: TokenPref
+        private lateinit var token: Token
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState:
+            Bundle?
+        ): View? = inflater.inflate(R.layout.fragment_global_quotes, container, false)
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+            val binding = FragmentGlobalQuotesBinding.bind(view)
+            binding.recyclerviewGlobalQuotes.layoutManager = LinearLayoutManager(activity)
+            tokenPref = TokenPref(requireActivity())
+            token = tokenPref.getToken()
+            adapter = QuoteAdapter(requireActivity())
+            binding.recyclerviewGlobalQuotes.adapter = adapter
+            presenter =
+                MainPresenter(this, CoroutineContextProvider())
+            progressbar.visibility = View.VISIBLE
+            presenter.getMyQuotes(token.token)
+            swiperefresh.onRefresh {
+                progressbar.visibility = View.INVISIBLE
+                presenter.getMyQuotes(token.token)
+            }
+        }
+        override fun onResume() {
+            super.onResume()
+            presenter.getAllQuotes(token.token.toString())
+        }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        notificationsViewModel =
-            ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_global_quotes, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        override fun showMessage(messsage: String) {
+            Toast.makeText(requireActivity(),messsage, Toast.LENGTH_SHORT).show()
+        }
+        override fun resultQuote(data: ArrayList<Quote>) {
+            quotes.clear()
+            adapter.listQuotes = data
+            quotes.addAll(data)
+            adapter.notifyDataSetChanged()
+            progressbar.visibility = View.INVISIBLE
+            swiperefresh.isRefreshing = false
+        }
+        override fun resultLogin(data: Login) {
+        }
     }
-}
+
+
